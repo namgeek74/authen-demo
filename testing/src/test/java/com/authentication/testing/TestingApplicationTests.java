fa5bfc21da313f7cc71b92a07c01e32de025c92d
namnd <namnguyennd7428@gmail.com>
2023-09-11 23:25:20 +0700

    init project

---
+++ testing/src/test/java/com/authentication/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
